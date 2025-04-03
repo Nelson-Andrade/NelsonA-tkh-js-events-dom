dd38867114e9cabaf5b8b42ca2a92fc3caf2123f
Nelson <nsandrad20@gmail.com>
2025-04-02 18:37:00 -0700

    Implemented event listeners to handle DOM events. Triggered function callbacks depending on type of event.

---
+++ script.js
@@ -0,0 +1,20 @@
+//When the dom is fully loaded, this will be fired
+document.addEventListener("DOMContentLoaded", function() {
+    //Get the element with an id of onload
+    let onLoad = document.getElementById("onload");
+    //Add some HTML within the element we grabbed
+    onLoad.innerHTML = "<h1>I loaded cause the DOM was fully loaded</h1>";
+});
+
+//Get the element that will show our key code
+let direction = document.getElementById("direction");
+//It Can be changed with Key press or key up depending on what you might need 
+document.addEventListener("keydown", function(e) {
+    // When a key is pressed, the element with the id of direction will show the message
+    direction.innerText = `The Key you pressed is ${e.code}`;
+});
+
+let button = document.getElementById("clickMe");
+document.addEventListener("click", function() {
+    button.innerHTML = "You clicked me!";
+}); 
